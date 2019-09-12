81b43edb1460ef4cc1f892cd26b19d7aa06154a1
Salo Charabati <salochara@gmail.com>
2019-09-12 17:37:31 -0500

    reverse list program working

--- reverseList.rkt
+++ reverseList.rkt
@@ -1,9 +1,16 @@
-
+#lang racket
+#| Function for reversing a list using recursion |#
+; Loops are accomplished in Racket using recursion
+; The debugger in DrRacket is helpful for understanding how this works
 
 (define (reverseList input)
-    (if input)            ; Case base
-
-    (cdr input)
-    (car input)
-
-)
+    (if (empty? input)
+        empty
+        (append
+            (reverseList(cdr input))
+            (list (car input))
+        )
+    )
+)           
+    
+(reverseList '(3 2 6 1))
